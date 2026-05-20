15179bda1e67e157d7911fd46d1f9f4cc9c34056
muehuehuehue <yungblud1997.dom@gmail.com>
2026-05-21 00:42:45 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/final/',
   plugins: [react()],
-  base: '/final/', // ✅ Replace with your actual repo name
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-  },
 })
